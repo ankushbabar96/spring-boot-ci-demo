fec841db6b632be471bf535b13607553a054aba4
ankushbabar96 <ankush.babar_comp22@pccoer.in>
2026-02-23 11:46:35 +0530

    Initial commit

---
+++ src/test/java/com/Hello/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hello.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
